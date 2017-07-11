8493f5587b4bfbd4d3e6d09ce61183a67b8037b3
jnandez <javendan@uwo.ca>
2017-07-11 15:35:23 -0400

    First commit to the mpi branch. The code is not MPI ready

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
+import mpi4py.MPI as mpi
+
 def greet():
-    print("Hello world!")
+    rank = mpi.COMM_WORLD.Get_rank()
+    size = mpi.COMM_WORLD.Get_size()
+    print("Hello from {0} of {1}!".format(rank,size))
 
 greet()
